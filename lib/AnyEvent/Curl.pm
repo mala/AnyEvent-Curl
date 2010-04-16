7c5b69eee5699ca03cc4cd00ab3d3bb43694a5d8
mala <github@ma.la>
2010-04-16 17:39:33 +0900

    initial commit

---
+++ lib/AnyEvent/Curl.pm
@@ -0,0 +1,33 @@
+package AnyEvent::Curl;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+AnyEvent::Curl -
+
+=head1 SYNOPSIS
+
+  use AnyEvent::Curl;
+
+=head1 DESCRIPTION
+
+AnyEvent::Curl is
+
+=head1 AUTHOR
+
+mala E<lt>cpan@ma.laE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
